cb41e34dbb670daa21105eb3f61f43fe741fb523
Jordan Ranson <jordan@jordanranson.com>
2013-04-02 23:25:34 -0600

    Made a cool web sockets touch events demo

---
+++ audio/app.js
@@ -0,0 +1,39 @@
+var app = require("http").createServer(handler);
+var path = require("path");
+var io = require("socket.io").listen(app);
+var fs = require("fs");
+
+app.listen(80);
+
+function handler (request, response) {
+    console.log("request starting...");
+     
+    var filePath = "." + request.url;
+    if (filePath == "./")
+        filePath = "./index.htm";
+     
+    path.exists(filePath, function(exists) {
+        if (exists) {
+            fs.readFile(filePath, function(error, content) {
+                if (error) {
+                    response.writeHead(500);
+                    response.end();
+                }
+                else {
+                    response.writeHead(200, { "Content-Type": "text/html" });
+                    response.end(content, "utf-8");
+                }
+            });
+        }
+        else {
+            response.writeHead(404);
+            response.end();
+        }
+    });
+}
+
+io.sockets.on("connection", function (socket) {
+    socket.on("send pos", function (data) {
+        socket.broadcast.emit("update pos", data);
+    });
+});
